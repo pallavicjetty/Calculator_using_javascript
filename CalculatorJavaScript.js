d045db9ee42e7318e115dc9adab57612ce2eeedd
Pallavi <45694834+pallavicjetty@users.noreply.github.com>
2019-03-20 15:29:57 +0530

    Add files via upload

---
+++ CalculatorJavaScript.js
@@ -0,0 +1,38 @@
+function GetNumber()
+{
+    a=document.getElementById("num1").value;
+    b=document.getElementById("num2").value;
+}
+function Add()
+{   GetNumber();
+     document.getElementById('Result').innerHTML = +a + +b;
+}
+
+function Sub()
+{
+    GetNumber();
+     document.getElementById('Result').innerHTML = a - b;
+}
+
+function Multiplcation()
+{
+    GetNumber();
+     document.getElementById('Result').innerHTML = a*b;
+}
+
+function Division()
+{
+    GetNumber();
+    
+    if(b===0)
+    {
+        console.log("infinity");
+    }
+else
+    { 
+     document.getElementById('Result').innerHTML = a/b;
+    }
+
+}
+
+
